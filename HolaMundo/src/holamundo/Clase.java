1d5405648e6c51954fc218d56eff3e7e90fe2655
203p21 <203p21@203p10-PC>
2017-02-24 09:13:45 +0100

    Primer cambio

---
+++ HolaMundo/src/holamundo/Clase.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo;
+
+/**
+ *
+ * @author 203p21
+ */
+public class Clase {
+    int a;
+    public Clase(){
+        a=7;
+    }
+}
